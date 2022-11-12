675dcb83bd0ea5b04dc3358403de89051cb67bd6
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-11-12 22:31:17 +0100

    Update env1.c

--- env1.c
+++ env1.c
@@ -53,3 +53,27 @@ char *_getenv(const char *name, char **_environ)
 
 	return (ptr_env + mov);
 }
+
+/**
+ * _env - prints the evironment variables
+ *
+ * @datash: data relevant.
+ * Return: 1 on success.
+ */
+int _env(data_shell *datash)
+{
+	int i, j;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+
+		for (j = 0; datash->_environ[i][j]; j++)
+			;
+
+		write(STDOUT_FILENO, datash->_environ[i], j);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	datash->status = 0;
+
+	return (1);
+}
