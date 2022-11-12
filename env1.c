09de138d000a0f8d2eb21594eb0b6f0e468af45b
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-11-12 22:30:54 +0100

    Update env1.c

--- env1.c
+++ env1.c
@@ -22,3 +22,34 @@ int cmp_env_name(const char *nenv, const char *name)
 
 	return (i + 1);
 }
+/**
+ * _getenv - get an environment variable
+ * @name: name of the environment variable
+ * @_environ: environment variable
+ *
+ * Return: value of the environment variable if is found.
+ * In other case, returns NULL.
+ */
+char *_getenv(const char *name, char **_environ)
+{
+	char *ptr_env;
+	int i, mov;
+
+	/* Initialize ptr_env value */
+	ptr_env = NULL;
+	mov = 0;
+	/* Compare all environment variables */
+	/* environ is declared in the header file */
+	for (i = 0; _environ[i]; i++)
+	{
+		/* If name and env are equal */
+		mov = cmp_env_name(_environ[i], name);
+		if (mov)
+		{
+			ptr_env = _environ[i];
+			break;
+		}
+	}
+
+	return (ptr_env + mov);
+}
