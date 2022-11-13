85ef68f962e31d2f49afd143700b77ffe2aae026
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-11-13 11:53:49 +0100

    Update cmd_exec.c

--- cmd_exec.c
+++ cmd_exec.c
@@ -71,3 +71,48 @@ char *_which(char *cmd, char **_environ)
 			return (cmd);
 	return (NULL);
 }
+
+/**
+ * is_executable - determines if is an executable
+ *
+ * @datash: data structure
+ * Return: 0 if is not an executable, other number if it does
+ */
+int is_executable(data_shell *datash)
+{
+	struct stat st;
+	int i;
+	char *input;
+
+	input = datash->args[0];
+	for (i = 0; input[i]; i++)
+	{
+		if (input[i] == '.')
+		{
+			if (input[i + 1] == '.')
+				return (0);
+			if (input[i + 1] == '/')
+				continue;
+			else
+				break;
+		}
+		else if (input[i] == '/' && i != 0)
+		{
+			if (input[i + 1] == '.')
+				continue;
+			i++;
+			break;
+		}
+		else
+			break;
+	}
+	if (i == 0)
+		return (0);
+
+	if (stat(input + i, &st) == 0)
+	{
+		return (i);
+	}
+	get_error(datash, 127);
+	return (-1);
+}
