f18fd7c9484943f5b31c984bba134ec3c1ea6612
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2022-11-13 11:58:01 +0100

    Create check_syntax_error.c

---
+++ check_syntax_error.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * repeated_char - counts the repetitions of a char
+ *
+ * @input: input string
+ * @i: index
+ * Return: repetitions
+ */
+int repeated_char(char *input, int i)
+{
+	if (*(input - 1) == *input)
+		return (repeated_char(input - 1, i + 1));
+
+	return (i);
+}
+
+/**
+ * error_sep_op - finds syntax errors
+ *
+ * @input: input string
+ * @i: index
+ * @last: last char read
+ * Return: index of error. 0 when there are no
+ * errors
+ */
+int error_sep_op(char *input, int i, char last)
+{
+	int count;
+
+	count = 0;
+	if (*input == '\0')
+		return (0);
+
+	if (*input == ' ' || *input == '\t')
+		return (error_sep_op(input + 1, i + 1, last));
+
+	if (*input == ';')
+		if (last == '|' || last == '&' || last == ';')
+			return (i);
+
+	if (*input == '|')
+	{
+		if (last == ';' || last == '&')
+			return (i);
+
+		if (last == '|')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	if (*input == '&')
+	{
+		if (last == ';' || last == '|')
+			return (i);
+
+		if (last == '&')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	return (error_sep_op(input + 1, i + 1, *input));
+}
